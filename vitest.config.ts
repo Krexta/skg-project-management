d5ee6d2f70bbc2780f2e318da18ae190fce95f4a
Krexta <sakuradev521@gmail.com>
2025-08-01 16:27:13 +0900

    #1 プロジェクトのスケルトンを作成する (#2)
    * feat: docker setup
    
    * feat: nestjs setup
    
    * feat: add devcontainer
    
    * feat: update eslint setup
    
    * feat: move files
    
    * feat: add custom logger
    
    * feat: update logger
    
    * feat: add app config
    
    * feat: add swagger
    
    * feat: add http tracker
    
    * feat: jest to vitest
    
    * feat: add testing logger
    
    * feat: add github actions
    
    * feat: add prisma and dbml
    
    * feat: add fast-check
    
    * feat: add gitkeep
    
    * feat: add postgraphile
    
    * chore: update
    
    * feat: add class validator
    
    * chore: update
    
    * chore: update readme
    
    * chore: update
    
    * feat: update
    
    * feat: prisma init
    
    * chore: update
    
    * Update .devcontainer.json

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+// cSpell:ignore lcov
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/domain/repository',
+        '**/utility',
+        '**/index.ts',
+        '**/*spec.ts',
+        'src/main.ts',
+        'src/module',
+        'src/infrastructure/service',
+        'src/presentation/middleware',
+      ],
+      reportsDirectory: './coverage',
+      reporter: ['text', 'html', 'lcov', 'json-summary', 'json'],
+    },
+  },
+});
